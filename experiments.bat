7c0b45135a4ee4bf3549be425bcaf06885c2ee21
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-07-29 07:02:44 +0200

    Update experiments

--- experiments.bat
+++ experiments.bat
@@ -1,8 +1,12 @@
-python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type complex
-python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type sine
-python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type complex
-python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type sine
-python run_simulation.py --num_simulations 1000 --n 4000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type complex
-python run_simulation.py --num_simulations 1000 --n 4000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type sine
-python run_simulation.py --num_simulations 1000 --n 4000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type complex
-python run_simulation.py --num_simulations 1000 --n 4000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type sine
+REM python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type complex
+REM python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type sine
+REM python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type complex
+REM python run_simulation.py --num_simulations 1000 --n 1000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 100 --n 10000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type complex
+python run_simulation.py --num_simulations 100 --n 10000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 100 --n 10000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type complex
+python run_simulation.py --num_simulations 100 --n 10000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 10 --n 100000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type complex
+python run_simulation.py --num_simulations 10 --n 100000 --p 100 --alpha 0.0165 --n_estimators 200 --n_jobs -1 --cate_type sine
+python run_simulation.py --num_simulations 10 --n 100000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type complex
+python run_simulation.py --num_simulations 10 --n 100000 --p 100 --alpha 0.1 --n_estimators 200 --n_jobs -1 --cate_type sine
